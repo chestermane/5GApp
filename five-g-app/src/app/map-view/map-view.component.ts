cf2aff3acf70a162b668ec05274a7cd75d30e5b4
kheu18 <kheu18@gmail.com>
2019-04-04 15:23:52 -0400

    add map

---
+++ five-g-app/src/app/map-view/map-view.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+declare let L;
+@Component({
+  selector: 'map-view',
+  templateUrl: './map-view.component.html',
+  styleUrls: ['./map-view.component.css']
+})
+export class MapViewComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+    const map = L.map('map').setView([34.055557, -84.277249], 15);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    const circle = L.circle([34.055557, -84.277249], {
+      color: 'green',
+      fillColor: '#10da20',
+      fillOpacity: 0.5,
+      radius: 500
+    }).addTo(map);
+  }
+}
